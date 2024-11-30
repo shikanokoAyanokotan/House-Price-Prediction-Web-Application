fee6e7bafc364f1cc199cc2563471838368b0e62
shikanokoAyanokotan <godof10a1@gmail.com>
2024-11-30 10:23:35 +0700

    updates models, requirements and API document

--- PredictHousePrice/decisionTree.py
+++ PredictHousePrice/decisionTree.py
@@ -8,8 +8,8 @@ def train_decision_tree(X_train, X_test, y_train, y_test):
     # Define the parameter grid
     param_grid = {
         'max_depth': [5, 10, 15],
-        'min_samples_split': [2, 5, 8],
-        'min_samples_leaf': [1, 2, 5, 8],
+        'min_samples_split': [6, 8, 10],
+        'min_samples_leaf': [3, 5, 8, 10],
         'max_features': ['log2', 'sqrt', 0.8],
         'max_leaf_nodes': [30, 50, 80], 
     }
